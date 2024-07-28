d21f5a64aa8009dbcbc7c5921af1528650d6bb7a
Nicholas Anttila <nicholas.anttila@gmail.com>
2024-07-28 19:44:17 +0300

    fixed websocket host url, fixed server files building twice in deploy

--- client/src/routes/Index.tsx
+++ client/src/routes/Index.tsx
@@ -14,14 +14,14 @@ import { dispatchToast } from "../components/Toasts/Toaster.js";
 import { aiGameboard } from "../lib/Gameboard/Gameboard.js";
 import { trpc } from "../trpc.js";
 
-const url = "ws://localhost:3000";
+const websocketURL =  process.env.WEBSOCKET_URL ?? "ws://localhost:3000";
 
 export function Index() {
 	const navigate = useNavigate();
 	const [name, setName] = useState("");
 	const suffix = useRef(Date.now().toString().slice(-3));
 
-	useWebSocket(url, {
+	useWebSocket(websocketURL, {
 		shouldReconnect: (_e: CloseEvent) => true,
 		filter: parseMessage,
 	});
