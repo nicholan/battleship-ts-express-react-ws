3457ed3e44cfecfcd893b0deb6fa0e255107aa5c
Nicholas Anttila <nicholas.anttila@gmail.com>
2024-07-28 19:53:03 +0300

    modified env config, cors for production

--- server/src/configs/config.ts
+++ server/src/configs/config.ts
@@ -8,13 +8,14 @@ const envSchema = z.object({
 	SECRET_KEY: z.string().min(1),
 	DATABASE_URI: z.string().min(1),
 	NODE_ENV: z.string().min(1),
+	WEBSOCKET_URL: z.string().min(1),
+	HOST_URL: z.string().min(1)
 });
 
 export type cfgSchema = z.infer<typeof envSchema>;
 
 type Config = cfgSchema | NodeJS.ProcessEnv;
 
-const _productionHosts: string[] = [];
 const _devHosts = [
 	"http://localhost:3000",
 	"http://localhost:5173",
@@ -23,12 +24,11 @@ const _devHosts = [
 
 export function createConfig(cfg: Config = process.env) {
 	const env = envSchema.parse(cfg);
-	const hosts = env.NODE_ENV === "dev" ? _devHosts : _productionHosts;
 	const config = {
 		IS_TESTING: env.NODE_ENV === "test",
 		IS_DEVELOPMENT: env.NODE_ENV === "dev",
 		API_PORT: env.API_PORT ? env.API_PORT : 8080,
-		ALLOWED_HOSTS: hosts,
+		ALLOWED_HOSTS: env.NODE_ENV === "prod" ? env.HOST_URL :_devHosts,
 		DATABASE_URI: env.DATABASE_URI,
 	};
 
